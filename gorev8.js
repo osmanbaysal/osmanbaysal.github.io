91f2e256b9640d724f385dd30f0167af43715cc8
Osman Baysal <osmanbaysal3@gmail.com>
2023-12-15 10:34:24 +0300

    aaaa

---
+++ gorev8.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var modal = document.getElementById("myModal");
+    var openModalBtn = document.getElementById("openModalBtn");
+    var closeModalBtn = document.getElementById("closeModalBtn");
+
+    openModalBtn.addEventListener("click", function() {
+        modal.style.display = "block";
+    });
+
+    closeModalBtn.addEventListener("click", function() {
+        modal.style.display = "none";
+    });
+
+    window.addEventListener("click", function(event) {
+        if (event.target === modal) {
+            modal.style.display = "none";
+        }
+    });
+});
